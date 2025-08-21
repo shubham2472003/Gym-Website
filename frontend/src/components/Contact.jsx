5fd029a2b8c4c5da3871f85580c1a1cb0bd3def2
Shubham Rawat <145898040+shubham2472003@users.noreply.github.com>
2025-08-21 08:59:21 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://gym-website-backend-8itx.onrender.com",
         {
           name,
           email,
